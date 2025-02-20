129b7d2af2125301061a5901ecb2d8af63b69697
Birband <maciejsztramski@gmail.com>
2025-02-20 15:01:56 +0100

    feat: make ants sniff some pheromones

--- backend/app/solvers/ant/ant_solver.py
+++ backend/app/solvers/ant/ant_solver.py
@@ -1,20 +1,41 @@
 import random
+import numpy as np
 from app.models.solve import License, LicenseAssignment, Assignments
 from app.models.graph import Graph, Node
 
+# Tylko to na razie działa tak, że:
+# 1. Mrówki rozpylają feromon na węzły, które pokrywają
+# 2. Feromon wpływa TYLKO na wybór węzła ( nie na wybór licencji )
+# 3. Wybór nalepiej pokrywającej licencji dla węzla
+
 
 class Ant:
-    def __init__(self, graph: Graph, licenses: list[License]):
+    def __init__(self, graph: Graph, licenses: list[License], pheromones: dict[Node, float], alpha: float, beta: float):
         self.graph = graph
         self.licenses = licenses
+        self.pheromones = pheromones
+        self.alpha = alpha
+        self.beta = beta
         self.solution: Assignments = {}
         self.covered_nodes = set()
         self.cost = 0
 
-    def select_license(self, node: Node):
-        """Wybiera licencję do zakupu na danym węźle"""
+    def choose_node(self, available_nodes: set[Node]) -> Node:
+        """Wybiera węzeł probabilistycznie na podstawie feromonów i heurystyki"""
+        nodes = list(available_nodes)
+        pheromone_values = np.array([self.pheromones[n] for n in nodes])
+        heuristic_values = np.array([len(self.graph.get_neighbors(n)) for n in nodes])
+
+        probabilities = (pheromone_values ** self.alpha) * (heuristic_values ** self.beta)
+        print(probabilities)
+        probabilities /= probabilities.sum()
+
+        return np.random.choice(nodes, p=probabilities)
+
+    def select_license(self, node: Node) -> tuple[License, list[Node]]:
+        """Wybiera licencję, biorąc pod uwagę pokrycie oraz feromony"""
         best_license = None
-        best_coverage = 0
+        best_score = -1
         best_covered_nodes = []
 
         for license in self.licenses:
@@ -22,19 +43,21 @@ class Ant:
             covered_nodes = list(candidate_set)[: license.limit]
             effective_coverage = len(set(covered_nodes) - self.covered_nodes)
 
-            if effective_coverage > best_coverage:
+            score = effective_coverage + self.pheromones[node]
+
+            if score > best_score:
                 best_license = license
-                best_coverage = effective_coverage
+                best_score = score
                 best_covered_nodes = covered_nodes
 
         return best_license, best_covered_nodes
 
-    def construct_solution(self):
+    def construct_solution(self) -> None:
         """Tworzy rozwiązanie, wybierając licencje dla węzłów"""
         available_nodes = set(self.graph.nodes)
 
         while available_nodes:
-            node = random.choice(list(available_nodes))
+            node = self.choose_node(available_nodes)  # Wybór węzła na podstawie feromonów
             license, covered_nodes = self.select_license(node)
 
             if license and covered_nodes:
@@ -50,17 +73,20 @@ class Ant:
             available_nodes -= self.covered_nodes
 
 
-def ant_solver(graph: Graph, licenses: list[License], ants: int, iterations: int) -> Assignments:
-    '''
+def ant_solver(graph: Graph, licenses: list[License], ants: int, iterations: int, alpha: float = 1.0, beta: float = 2.0, evaporation: float = 0.9) -> Assignments:
+    """
     Rozwiązanie problemu pokrycia węzłów grafu licencjami przy użyciu algorytmu mrówkowego.
 
     :param graph: graf, którego węzły mają być pokryte licencjami
     :param licenses: lista dostępnych licencji
     :param ants: liczba mrówek
     :param iterations: liczba iteracji
+    :param alpha: wpływ feromonów na wybór
+    :param beta: wpływ heurystyki (liczba sąsiadów) na wybór
+    :param evaporation: stopień parowania feromonów
 
     :return: przypisanie licencji do węzłów
-    '''
+    """
     pheromones = {node: 1.0 for node in graph.nodes}
     best_solution = None
     best_cost = float("inf")
@@ -68,20 +94,21 @@ def ant_solver(graph: Graph, licenses: list[License], ants: int, iterations: int
     for _ in range(iterations):
         solutions = []
         for _ in range(ants):
-            ant = Ant(graph, licenses)
+            ant = Ant(graph, licenses, pheromones, alpha, beta)
             ant.construct_solution()
             solutions.append(ant)
 
         solutions.sort(key=lambda ant: ant.cost)
+        best_ant = solutions[0]
 
-        if solutions[0].cost < best_cost:
-            best_solution = solutions[0].solution
-            best_cost = solutions[0].cost
+        if best_ant.cost < best_cost:
+            best_solution = best_ant.solution
+            best_cost = best_ant.cost
 
         for node in pheromones:
-            pheromones[node] *= 0.9
-        for _, assignments in solutions[0].solution.items():
+            pheromones[node] *= evaporation
+        for _, assignments in best_ant.solution.items():
             for assignment in assignments:
-                pheromones[assignment.license_holder] += 1.0 / solutions[0].cost # nie wiem czy to jest najlepsze rozwiązanie ale to się zobaczy jak będziemy porównywać wyniki :)
+                pheromones[assignment.license_holder] += 1.0 / best_ant.cost # :)
 
     return best_solution if best_solution else {}
