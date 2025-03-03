206da8ced9f21165953e9012353f65bf566fc737
ack2406 <ack9222@gmail.com>
2025-03-03 12:16:52 +0100

    feat: add CPSATSolver implementation and update exports

--- src/solvers/static/__init__.py
+++ src/solvers/static/__init__.py
@@ -2,5 +2,6 @@ from .ant_colony import AntColonySolver
 from .greedy import GreedySolver
 from .mip import MIPSolver
 from .tabu_search import TabuSolver
+from .cp_sat import CPSATSolver
 
-__all__ = ["AntColonySolver", "GreedySolver", "TabuSolver", "MIPSolver"]
+__all__ = ["AntColonySolver", "GreedySolver", "TabuSolver", "MIPSolver", "CPSATSolver"]
