a231d430bac09dfad8aa28d3ec0194aad3df6dac
ack2406 <ack9222@gmail.com>
2025-08-25 18:58:03 +0200

    fix: update import path for GraphVisualizer in __init__.py

--- src/graphs/__init__.py
+++ src/graphs/__init__.py
@@ -1,4 +1,4 @@
-from .visualization import GraphVisualizer
+from src.viz.graph_visualizer import GraphVisualizer
 from .data_loader import RealWorldDataLoader
 from .dynamic_simulator import DynamicNetworkSimulator, DynamicScenarioFactory, MutationParams
 
